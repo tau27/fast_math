fd8e296455a7173776baff6bd48173305d33c539
djamalArch <lazeykin07@gmail.com>
2022-06-02 20:00:27 +0300

    Prepare del mgt

--- Test.py
+++ Test.py
@@ -4,10 +4,7 @@ from Media import *
 from os import path
 from Templ import *
 
-name = path.splitext(os.path.basename(__file__))[0]
-print(name)
 class SusTest(MScene):
     def construct(self):
-        config["background_color"] = WHITE
-        text = MathTex(r"\mathbb{TRIVIAL}", color = BLACK).scale(5)
-        self.add(text)
+        text = MathTex(r"\mathbb{TRIVIAL}").scale(5)
+        self.play(Write(text))
