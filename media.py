87e2be6e3613735af3e1169f07dbb759a057bcc3
djamalArch <lazey07@proton.me>
2022-09-13 23:44:12 +0300

    RM upd

--- media.py
+++ media.py
@@ -2,7 +2,10 @@ from emanim import *
 
 class Banner(Scene):
     def construct(self):
-        text = Tex("Fast", " ", "Math")
-        text.set_color_by_tex_to_color_map({"Fast" : "#56E3A7","Math" : "#58C4DD"})
-        text.scale(5)
-        self.add(text)
+        title = Tex("Fast", " ", "Math").shift(UP)
+        title.set_color_by_tex_to_color_map({"Fast" : "#56E3A7","Math" : "#58C4DD"})
+        title.scale(5)
+
+        quote = Tex("Wir müssen wissen,", " ", "wir werden wissen!").shift(DOWN*2)
+
+        self.add(title, quote)
